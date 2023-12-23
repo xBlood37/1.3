79f56c0c80fbeb9389eb57b89cf16fae61b86586
xBlood37 <123d.123b@mail.ru>
2023-12-23 14:00:10 +0300

    swiper slider fix

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,6 @@ const brandsInner = document.querySelector('.brands__inner ');
 
 brandsBtn.addEventListener('click', () => {
   brandsInner.classList.toggle('brands__inner--visible');
+  if (brandsInner.classList.contains('brands__inner--visible')) brandsBtn.textContent = 'Скрыть';
+  if (!brandsInner.classList.contains('brands__inner--visible')) brandsBtn.textContent = 'Показать все';
 });
